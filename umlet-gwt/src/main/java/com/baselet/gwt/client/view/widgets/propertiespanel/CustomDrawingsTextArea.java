473b254b54a12938f79427283a5b7e56cd4b8fb6
Tsvetan Tsvetkov <e1328061@student.tuwien.ac.at>
2021-03-31 15:22:49 +0200

    Refactor CustomDrawingsTextArea and PropertiesTextArea - code is almost the same.

--- umlet-gwt/src/main/java/com/baselet/gwt/client/view/widgets/propertiespanel/CustomDrawingsTextArea.java
+++ umlet-gwt/src/main/java/com/baselet/gwt/client/view/widgets/propertiespanel/CustomDrawingsTextArea.java
@@ -1,116 +1,18 @@
 package com.baselet.gwt.client.view.widgets.propertiespanel;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.baselet.element.interfaces.HasPanelAttributes;
-import com.baselet.gui.AutocompletionText;
-import com.baselet.gwt.client.logging.CustomLogger;
-import com.baselet.gwt.client.logging.CustomLoggerFactory;
-import com.baselet.gwt.client.view.DrawPanelDiagram;
-import com.baselet.gwt.client.view.interfaces.Redrawable;
 import com.baselet.gwt.client.view.widgets.OwnTextArea;
-import com.baselet.gwt.client.view.widgets.OwnTextArea.InstantValueChangeHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyDownEvent;
-import com.google.gwt.event.dom.client.KeyDownHandler;
 import com.google.gwt.uibinder.client.UiConstructor;
 
-public class CustomDrawingsTextArea extends MySuggestBox {
-
-	private static final String DEFAULT_HELPTEXT = "Space for custom drawings code";
-
-	private final OwnTextArea textArea;
-
-	private final MySuggestOracle oracle;
-
-	private HasPanelAttributes gridElement;
-
-	private Redrawable activePanel = null;
-
-	private String oldText;
+public class CustomDrawingsTextArea extends TextArea {
 
 	@UiConstructor
 	public CustomDrawingsTextArea() {
-		this(new MySuggestOracle(), new OwnTextArea());
-
-		textArea.setInstantValueChangeHandler(new InstantValueChangeHandler() {
-			@Override
-			public void onValueChange(String value) {
-				if (gridElement != null) {
-					gridElement.setCustomDrawingsCode(getValue());
-				}
-				activePanel.redraw();
-
-				if (oldText != null && !oldText.equals(value)) {
-					oldText = value;
-
-					// Update the file-in progress for vscode on any value changes
-					if (activePanel instanceof DrawPanelDiagram) {
-						((DrawPanelDiagram) activePanel).handleFileUpdate();
-					}
-				}
-			}
-		});
-
-		textArea.addKeyDownHandler(new KeyDownHandler() { // CTRL+Space shows all suggestions
-			@Override
-			public void onKeyDown(KeyDownEvent event) {
-				int key = event.getNativeEvent().getKeyCode();
-				if (key == KeyCodes.KEY_TAB) {
-					event.getNativeEvent().preventDefault();
-					int cursorPos = textArea.getCursorPos();
-					int cursorPosLine = getCursorPositionInLine();
-					String currentTextLine = getCursorLine();
-					setText(currentTextLine.substring(0, cursorPosLine) + "\t" + currentTextLine.substring(cursorPosLine));
-					textArea.setCursorPos(cursorPos + 1);
-				}
-			}
-		});
+		super();
 	}
 
 	public CustomDrawingsTextArea(final MySuggestOracle oracle, OwnTextArea textArea) {
 		super(oracle, textArea);
-		this.oracle = oracle;
-		this.textArea = textArea;
-	}
-
-	public void setGridElement(HasPanelAttributes panelAttributeProvider, Redrawable panel) {
-		activePanel = panel;
-		gridElement = panelAttributeProvider;
-		String customDrawingsCode = panelAttributeProvider.getCustomDrawingsCode();
-		if (customDrawingsCode == null) {
-			customDrawingsCode = DEFAULT_HELPTEXT;
-		}
-		textArea.setValue(customDrawingsCode);
-		oldText = customDrawingsCode;
-		List<AutocompletionTextGwt> autocompletionTextList = new ArrayList<>();
-		for (AutocompletionText oldText : panelAttributeProvider.getAutocompletionList()) {
-			AutocompletionTextGwt newText = new AutocompletionTextGwt(oldText.getText(), oldText.getInfo(), oldText.getBase64Img());
-			autocompletionTextList.add(newText);
-		}
-		oracle.setAutocompletionList(autocompletionTextList);
-	}
-
-	private int getCursorPositionInLine() {
-		String wholeText = textArea.getText();
-		int cursorPos = textArea.getCursorPos();
-		int returnPos = 0;
-		if (!wholeText.contains("\n")) {
-			return cursorPos;
-		}
-		else {
-			int currentPos = 0;
-			for (String line : wholeText.split("(\r)?\n")) {
-				int oldPos = currentPos;
-				currentPos += line.length() + 1;
-				if (cursorPos < currentPos) {
-					returnPos = cursorPos - oldPos;
-					break;
-				}
-			}
-		}
-		return returnPos;
 	}
 
 	/**
@@ -120,5 +22,15 @@ public class CustomDrawingsTextArea extends MySuggestBox {
 	public void setText(String text) {
 		super.setText(text);
 		textArea.fireHandler();
+	}
+
+	@Override
+	public String getGridElementAttributes(HasPanelAttributes panelAttributeProvider) {
+		return panelAttributeProvider.getCustomDrawingsCode();
+	}
+
+	@Override
+	public void setGridElementAttributes() {
+		gridElement.setCustomDrawingsCode(getValue());
 	}
 }
